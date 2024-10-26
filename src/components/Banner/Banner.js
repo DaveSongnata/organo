899dd60ff502db240c07969467cad6fb667ba685
Dave <contatodrss@gmail.com>
2024-10-26 19:45:01 -0400

    feat: Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+    <header className = "banner">
+    <img src="/imagens/logo192.png" alt="banner principal"/>
+    </header>
+)
+
+} 
+
+
+
+
+
+export default Banner
