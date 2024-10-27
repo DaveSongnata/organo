9c009a83604179873b49c03ff118b13d8e277ee4
Dave <contatodrss@gmail.com>
2024-10-27 18:23:24 -0400

    feat: botao

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,5 +1,6 @@
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 import './Formulario.css'
 const Formulario = () => {
 
@@ -13,15 +14,24 @@ const Formulario = () => {
             'Inovação e Gestão',
         ]
     
+        var i = 0;   
+const aoSalvar = (evento) => {
+i = i+1;
+        console.log('thanx'+i)
+
+        evento.preventDefault()
+
+}
 
     return (
         <section className = "formulario">
-            <form>
+            <form onSubmit = {aoSalvar} >
                 <h2>Preencha os dados para criar os cards do Colaborador</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu Nome..." />
                 <CampoTexto label="Cargo" placeholder="Digite seu Cargo..." />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
                 <ListaSuspensa label = "Time" itens = {times}/>
+                <Botao texto = "Criar Card"/>
             </form>
         </section>
 
