2561cf0ea59eb51c2b10b20573068f92a0045cdd
Dave <contatodrss@gmail.com>
2024-10-26 21:58:14 -0400

    feat: ListaSuspensa

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,19 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () => {
+
+
+    const times = [
+            'Programação',
+            'Front-end',
+            'Data-Science',
+            'UX e Design',
+            'Mobile',
+            'Inovação e Gestão',
+        ]
+    
+
     return (
         <section className = "formulario">
             <form>
@@ -8,7 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu Nome..." />
                 <CampoTexto label="Cargo" placeholder="Digite seu Cargo..." />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-
+                <ListaSuspensa label = "Time" itens = {times}/>
             </form>
         </section>
 
