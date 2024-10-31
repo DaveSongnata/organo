f1adb587b0886471c83e26191eefaf6f25c8853c
Dave <contatodrss@gmail.com>
2024-10-31 13:17:06 -0400

    fix: background color now works properly!

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,11 +1,11 @@
 import './Colaborador.css'
 
 
-const Colaborador = ({nome, imagem, cargo}) => {
+const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
 
 return (
     <div className = 'colaborador'> 
-    <div className='cabecalho'> <img src = {imagem} alt = {nome}></img> </div>
+    <div className='cabecalho' style = {{backgroundColor: corDeFundo}}> <img src = {imagem} alt = {nome} ></img> </div>
     <div className = 'rodape'> 
         <h4>{nome}</h4>
         <h5>{cargo}</h5>
